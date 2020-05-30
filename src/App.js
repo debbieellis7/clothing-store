db55218c68bd36d511044fa745e1ea5eb70fbe6e
Debbie <debbieellis7@gmail.com>
2020-05-30 11:36:59 +0800

    google sign-in authentication 2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,40 @@ import Home from './components/Pages/Home/Home'
 import Shop from './components/Pages/Shop/Shop'
 import SignUpAndSignIn from './components/Pages/SignUp-SignIn/SignUp-SignIn'
 import Header from './components/Header/Header'
+import { auth } from './firebase/utils'
 import './App.css'
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/shop' component={Shop} />
-        <Route path='/signin' component={SignUpAndSignIn} />
-      </Switch>
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    currentUser: null
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount () {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user })
+
+      console.log(user)
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/shop' component={Shop} />
+          <Route path='/signin' component={SignUpAndSignIn} />
+        </Switch>
+      </div>
+    )
+  }
 }
 
 export default App
