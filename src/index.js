f3aee19ac0e813a0529dd3c8fe5e191fc6e26d91
Debbie <debbieellis7@gmail.com>
2020-06-01 17:04:33 +0800

    setting-up redux 1

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+ReactDOM.render (
+  <Provider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 )
